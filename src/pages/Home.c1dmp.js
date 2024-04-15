d9777d1f66d8bf1c34379f1c972d6383e432f488
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2024-04-15 07:37:25 +0000

    Initial commit

---
+++ src/pages/Home.c1dmp.js
@@ -0,0 +1,9 @@
+/**
+*	Adds an event handler that runs when the element is clicked.
+	[Read more](https://www.wix.com/corvid/reference/$w.ClickableMixin.html#onClick)
+*	 @param {$w.MouseEvent} event
+*/
+export function section2_click(event) {
+    // This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+    // Add your code for this event here: 
+}
